7020a46ef968efbb7d27d7f6f62de35687fef680
Rasul Agasiyev <79022070+rvsul@users.noreply.github.com>
2022-07-24 17:11:56 +0400

    Add files via upload

--- src/features/car/carSlice.js
+++ src/features/car/carSlice.js
@@ -1,7 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  cars: ["Model S", "Model 3", "Model X", "Model Y"],
+  cars: [
+    "Model S",
+    "Model 3",
+    "Model X",
+    "Model Y",
+    "Solar Roof",
+    "Solar Panels",
+  ],
 };
 
 const carSlice = createSlice({
@@ -11,5 +18,4 @@ const carSlice = createSlice({
 });
 
 export const selectCars = (state) => state.car.cars;
-
 export default carSlice.reducer;
